057c6ed7309ab3cef5616206ca248e532621980f
João Santos <62022855+devbyjoaos@users.noreply.github.com>
2024-08-25 10:49:21 -0300

    Adicionando mensagem de erro e exibição decimal

--- Binary_Divider.vhd
+++ Binary_Divider.vhd
@@ -66,7 +66,34 @@ architecture Behavioral of Divisor4Bits is
                 when "0111" => return "1111000"; -- 7
                 when "1000" => return "0000000"; -- 8
                 when "1001" => return "0010000"; -- 9
-                when others => return "1111111"; -- Error (não deve ocorrer para 4 bits)
+                when "1010" => return "1000000"; -- 0
+                when "1011" => return "1111001"; -- 1
+                when "1100" => return "0100100"; -- 2
+                when "1101" => return "0110000"; -- 3
+                when "1110" => return "0011001"; -- 4
+                when others => return "0010010"; -- 5
+            end case;
+        end function;
+
+   function BinTo7SegDec(val: std_logic_vector(3 downto 0)) return std_logic_vector is
+        begin
+            case val is
+                when "0000" => return "1000000"; -- 0
+                when "0001" => return "1000000"; -- 1
+                when "0010" => return "1000000"; -- 2
+                when "0011" => return "1000000"; -- 3
+                when "0100" => return "1000000"; -- 4
+                when "0101" => return "1000000"; -- 5
+                when "0110" => return "1000000"; -- 6
+                when "0111" => return "1000000"; -- 7
+                when "1000" => return "1000000"; -- 8
+                when "1001" => return "1000000"; -- 9
+                when "1010" => return "1111001"; -- 0
+                when "1011" => return "1111001"; -- 1
+                when "1100" => return "1111001"; -- 2
+                when "1101" => return "1111001"; -- 3
+                when "1110" => return "1111001"; -- 4
+                when others => return "1111001"; -- 5
             end case;
         end function;
 
@@ -95,13 +122,21 @@ begin
             Erro <= '0';  -- Divisão válida
         end if;
     end process;
+   
+    if(Erro == '1')
+       HEX0 <= "0000001";                -- Dígito das unidades do quociente
+       HEX1 <= "1001111"            -- Display apagado (ou pode ser adaptado para outro dígito)
 
-    -- Exibir quociente nos displays HEX0 e HEX1
-    HEX0 <= BinTo7Seg(Quociente(3 downto 0));  -- Dígito das unidades do quociente
-    HEX1 <= "1111111";                         -- Display apagado (ou pode ser adaptado para outro dígito)
+       -- Exibir resto nos displays HEX2 e HEX3
+       HEX2 <= "1001111"      -- Dígito das unidades do resto
+       HEX3 <= "0110000";
+    ELSE
+       -- Exibir quociente nos displays HEX0 e HEX1
+       HEX0 <= BinTo7Seg(Quociente(3 downto 0));                  -- Dígito das unidades do quociente
+       HEX1 <= BinTo7SegDec(Quociente(3 downto 0));             -- Display apagado (ou pode ser adaptado para outro dígito)
 
-    -- Exibir resto nos displays HEX2 e HEX3
-    HEX2 <= BinTo7Seg(Resto(3 downto 0));      -- Dígito das unidades do resto
-    HEX3 <= "1111111";                         -- Display apagado (ou pode ser adaptado para outro dígito)
-    
+       -- Exibir resto nos displays HEX2 e HEX3
+       HEX2 <= BinTo7Seg(Resto(3 downto 0));      -- Dígito das unidades do resto
+       HEX3 <= BinTo7SegDec(Resto(3 downto 0));                         -- Display apagado (ou pode ser adaptado para outro dígito)
+    END IF    
 end Behavioral;
