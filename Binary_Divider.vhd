b8540e716d118136762cc786178896217f004b84
João Santos <62022855+devbyjoaos@users.noreply.github.com>
2024-08-25 10:50:33 -0300

    Entity Binary_Divider

--- Binary_Divider.vhd
+++ Binary_Divider.vhd
@@ -1,40 +1,10 @@
--- Baseado no código de FPGA Prototyping by VHDL Examples - Pong P. Chu
--- Adaptacao para a placa DE1 da Altera
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity Binary_Divider is
-   port(
-      SW: in std_logic_vector(3 downto 0);
-      --dp: in std_logic;
-      HEX0: out std_logic_vector(6 downto 0)
-   );
-end Binary_Divider;
-
-architecture arch of Binary_Divider is
-begin
-   with SW select
-      HEX0(6 downto 0) <=
-         "1000000" when "0000",
-         "1111001" when "0001",
-         "0100100" when "0010",
-         "0110000" when "0011",
-         "0011001" when "0100",
-         "0010010" when "0101",
-         "0100000" when "0110",
-         "0001111" when "0111",
-         "0000000" when "1000",
-         "0000100" when "1001", --e       -- ERRADO !!! CORRIGIR !!!
-         "0111000" when others;
-end arch;
-
 library ieee;
 use ieee.std_logic_1164.all;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.numeric_std.all;
 
-entity Divisor4Bits is
+entity Binary_Divider is
     GENERIC(n: INTEGER := 3);
     Port (
         A: in std_logic_vector(3 downto 0);
@@ -45,9 +15,9 @@ entity Divisor4Bits is
         HEX3 : out std_logic_vector(6 downto 0);
         Erro : out std_logic
     );
-end Divisor4Bits;
+end Binary_Divider;
 
-architecture Behavioral of Divisor4Bits is
+architecture Behavioral of Binary_Divider is
 
     signal Quociente : std_logic_vector(3 downto 0);
     signal Resto : std_logic_vector(3 downto 0);
